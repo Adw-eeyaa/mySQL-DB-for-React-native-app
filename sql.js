3e0bcd9b38cab05f80a421dec69cdf4c993c10d6
Adw-eeyaa <yoyoveivoi@gmail.com>
2024-07-09 14:32:32 +0530

    env variables

--- sql.js
+++ sql.js
@@ -1,40 +1,41 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-const mysql = require("mysql2");
-const pool = mysql.createPool({
+    const express = require("express");
+    const app = express();
+    require('dotenv').config();
+    app.use(express.json());
+    const mysql = require("mysql2");
+    const pool = mysql.createPool({
 
-    host:"127.0.0.1",
-    user:"root",
-    password:"AdwaithDesuGaSQL334",
-    database:"userLogin"
-});
+        host:process.env.SQL_HOST,
+        user:process.env.SQL_USER,
+        password:process.env.SQL_PASSWORD,
+        database:process.env.SQL_DATABASE
+    });
 
 
-pool.query(`select * from loginInfo where username="Adwaith"`,(err,result) => {
-    if(err){
-        console.log(err);
-    }
-    else{
-        return console.log(result);
-    }
-});
-
-app.post('/login',(req,res)=>{
-    var username = req.body.username;
-    var password = req.body.password;
-    pool.query(`insert into loginInfo values("${username}","${password}")`,(err , result) =>{
+    pool.query(`select * from loginInfo where username="Adwaith"`,(err,result) => {
         if(err){
             console.log(err);
         }
         else{
-            return console.log({status:"ok"});
+            return console.log(result);
         }
     });
-})
-app.listen(6090,()=>{
-    console.log("server listening on port 6090")
-});
+
+    app.post('/login',(req,res)=>{
+        var username = req.body.username;
+        var password = req.body.password;
+        pool.query(`insert into loginInfo values("${username}","${password}")`,(err , result) =>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                return console.log({status:"ok"});
+            }
+        });
+    })
+    app.listen(6090,()=>{
+        console.log("server listening on port 6090")
+    });
 
 
 
